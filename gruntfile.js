588b58340da3fe0bb39303e3abf4ee6a8a06483f
Caleb Jonasson <caleb.jonasson@gmail.com>
2015-02-02 23:58:21 -0800

    updating the build process

--- gruntfile.js
+++ gruntfile.js
@@ -6,13 +6,114 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp/*',
+            '<%= pkg.dist %>/{,*/}*',
+            //'<%= pkg.dist %>/*',
+            '!<%= pkg.dist %>/.git{,*/}*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    concat: {
+      generated: {
+        files: [
+          {
+            dest: '.tmp/concat/js/app.js',
+            src: [
+              'bower_components/{,*/}*.min.js',
+              '<%= pkg.app %>/app.js',
+              '<%= pkg.app %>/pages/{,*/}*.js'
+            ]
+          }
+        ]
+      }
+    },
+    ngAnnotate: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/concat/js/app.js',
+          src: ['.'],
+          dest: '.tmp/concat/js/app.js'
+        }]
+      }
+    },
+    usemin: {
+      html: ['<%= pkg.dist %>/{,*/}*.html'],
+      css: ['<%= pkg.dist %>/assets/styles/{,*/}*.css'],
+      options: {
+        assetsDirs: ['<%= pkg.dist %>','<%= pkg.dist %>/images']
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'assets/icons',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= pkg.dist %>/images'
+        }]
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= pkg.app %>',
+          dest: '<%= pkg.dist %>/app',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            '*.html',
+            'pages/{,*/}*.html',
+            'core/{,*/}*.html',
+            'images/{,*/}*.{webp}',
+            'fonts/{,*/}*.*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.',
+          dest: '<%= pkg.dist %>/',
+          src: ['index.html']
+        },{
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= pkg.dist %>/images',
+          src: ['generated/*']
+        }, {
+          expand: true,
+          cwd: '.',
+          src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
+          dest: '<%= pkg.dist %>/assets/css'
+        },{
+          expand: true,
+          cwd: '<%= pkg.app %>',
+          src: '[**].js',
+          dest: '<%= pkg.dist %>/assets/js'
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= pkg.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: '.tmp/concat/js/app.js',
+        dest: '<%= pkg.dist %>/<%= pkg.app %>/app.js'
       }
     },
     sass: {
@@ -21,7 +122,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'assets/css/style.css': 'assets/css/main.scss'
+          '<%= pkg.dist %>/assets/css/style.css': 'assets/css/main.scss'
         }
       }
     }
@@ -31,6 +132,15 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', [
+    'clean',
+    'copy:dist',
+    'concat',
+    'ngAnnotate',
+    'uglify',
+    'sass',
+    'usemin',
+    'imagemin'
+  ]);
+  //grunt.registerTask('default', ['sass']);
 };
